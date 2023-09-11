355f88320916e20470e83ea8c4aef5008eda48b1
Francis Kornu <fkay0450@gmail.com>
2023-09-11 00:07:48 +0000

    feat: adding dialog component to order page

---
+++ src/components/customDialog.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Dialog } from '@headlessui/react';
+
+const CustomDialog = ({ isOpen, onClose, title, content }) => {
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+
+          {/* This is the dialog itself */}
+          <div className="inline-block mt-44 align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                    {title}
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-lg text-gray-500">{content}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                onClick={onClose}
+                type="button"
+                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-yellow-600 text-base font-medium text-white hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default CustomDialog;
--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -4,18 +4,43 @@ import { useEffect } from "react";
 import { ShowToast } from "../components/showToast";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"
+import CustomDialog from "../components/customDialog";
 
 export default function Gallery(){
   const [allOrders, setAllOrders] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openDialog = () => {
+    console.log("CLICKED OPEN")
+    setIsOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsOpen(false);
+  };
+
+  const PER_PAGE = 10;
+  const offset = currentPage * PER_PAGE;
 
   const { getOrders } = useFunctions();
   const router = useNavigate()
+  
+  const getDateString = (value) => {
+    const dateObj = new Date(value);
+    const month = dateObj.toLocaleString('default', { month: 'long' });
+    const day = dateObj.getDate();
+    const year = dateObj.getFullYear();
+
+    const formattedDate = `${month} ${day}, ${year}`;
+
+    return formattedDate
+  }
 
   const getCustomerOrders = async () => {
     const { response_code, orders } = await getOrders();
     if (response_code === 200){
-      console.log("ORDERS " + JSON.stringify(orders))
       setAllOrders(orders)
     }else if (response_code === 300){
       setIsLoading(false)
@@ -28,6 +53,48 @@ export default function Gallery(){
     }
   }
 
+  const getElemenetStatus = (e) => {
+    if (e){
+      return 'Shipped'
+    }else{
+      return 'Pending Shipping'
+    }
+  }
+
+  const ordersPageData = !!allOrders && allOrders.slice(offset, offset + PER_PAGE).map((data, index) => {
+    const orderTitle = `Order ID: ${data.reference_no}`;
+    const orderDetails = `Products: \n # ${data.orderItems[0].products.name} x${data.orderItems[0].quantity}`;
+    return (
+      <tr className="border-b dark:border-neutral-500">
+        <td className="whitespace-nowrap px-6 py-4 font-medium">{index + 1}</td>
+        <td className="whitespace-nowrap px-6 py-4">{getDateString(data.createdAt)}</td>
+        <td className="whitespace-nowrap px-6 py-4">{data.reference_no}</td>
+        <td className="whitespace-nowrap px-6 py-4">${data.amount}</td>
+        <td className="whitespace-nowrap px-6 py-4">{data.quantity}</td>
+        <td className="whitespace-nowrap px-6 py-4">{getElemenetStatus(data.status)}</td>
+        
+        <td className="whitespace-nowrap px-6 py-4">
+          <svg onClick={openDialog} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+
+          <CustomDialog
+            isOpen={isOpen}
+            onClose={closeDialog}
+            title={orderTitle}
+            content={
+            <div>
+              {orderDetails.split('\n').map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+            </div>}
+          />
+        </td>
+      </tr>
+    )
+  });
+
   useEffect(()=>{
     getCustomerOrders()
   },[])
@@ -35,43 +102,34 @@ export default function Gallery(){
   return(
     <div className="min-h-screen relative">
       <Navbar/>
-      <main className="main-home-content">
-      
+      <main className="main-home-content mt-8">
+      <div className="grid mt-12">
+        <div className="flex flex-row space-x-8">
+          <h1 className="header-colored-text header-colored-text-alt-2">Orders</h1>
+          <h1 className="cart-number">count: #{allOrders.length}</h1>
+        </div>
+      </div>
       {allOrders.length > 0 ? 
         allOrders.map((order_item)=>{
           return (
             <div className="flex flex-col">
-              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="overflow-x-auto">
                 <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                   <div className="overflow-hidden">
                     <table className="min-w-full text-left text-sm font-light">
                       <thead className="border-b font-medium dark:border-neutral-500">
                         <tr>
                           <th scope="col" className="px-6 py-4">#</th>
-                          <th scope="col" className="px-6 py-4">First</th>
-                          <th scope="col" className="px-6 py-4">Last</th>
-                          <th scope="col" className="px-6 py-4">Handle</th>
+                          <th scope="col" className="px-6 py-4">Date</th>
+                          <th scope="col" className="px-6 py-4">Order ID</th>
+                          <th scope="col" className="px-6 py-4">Amount</th>
+                          <th scope="col" className="px-6 py-4">No. of Items</th>
+                          <th scope="col" className="px-6 py-4">Status</th>
+                          <th scope="col" className="px-6 py-4">Other</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr className="border-b dark:border-neutral-500">
-                          <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                          <td className="whitespace-nowrap px-6 py-4">Mark</td>
-                          <td className="whitespace-nowrap px-6 py-4">Otto</td>
-                          <td className="whitespace-nowrap px-6 py-4">@mdo</td>
-                        </tr>
-                        <tr className="border-b dark:border-neutral-500">
-                          <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
-                          <td className="whitespace-nowrap px-6 py-4">Jacob</td>
-                          <td className="whitespace-nowrap px-6 py-4">Thornton</td>
-                          <td className="whitespace-nowrap px-6 py-4">@fat</td>
-                        </tr>
-                        <tr className="border-b dark:border-neutral-500">
-                          <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
-                          <td className="whitespace-nowrap px-6 py-4">Larry</td>
-                          <td className="whitespace-nowrap px-6 py-4">Wild</td>
-                          <td className="whitespace-nowrap px-6 py-4">@twitter</td>
-                        </tr>
+                        {!!allOrders && ordersPageData}
                       </tbody>
                     </table>
                   </div>
@@ -81,7 +139,7 @@ export default function Gallery(){
           )
         })
         :
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center mt-24">
           <h1 className="header-colored-text header-colored-text-alt-2">No Items</h1>
         </div>
       }
